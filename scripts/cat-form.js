625ed618efbd88a84adc7f07bd9cdf3f4574e9a9
Thomas VERHOKEN <tverhoken@norauto.com>
2018-10-06 10:47:31 +0200

    Add form validation check on cat card creation

--- scripts/cat-form.js
+++ scripts/cat-form.js
@@ -1,21 +1,27 @@
 import { createCard } from './data-service.js';
-import { setElementContent, getElement, addClass } from './dom-helper.js';
+import { setElementContent, getElement, addClass, removeClass } from './dom-helper.js';
 
 getElement('#cardForm').addEventListener('submit', e => {
   e.preventDefault();
+  const form = e.target;
 
-  const title = getElement('#cardTitle').value;
-  const imageUrl = getElement('#cardImage').value;
-  const description = getElement('#cardDescription').value;
-  const card = {
-    title,
-    imageUrl,
-    description
-  };
+  removeClass(form, 'was-validated');
+  if (!form.checkValidity()) {
+    addClass(form, 'was-validated');
+  } else {
+    const title = getElement('#cardTitle').value;
+    const imageUrl = getElement('#cardImage').value;
+    const description = getElement('#cardDescription').value;
+    const card = {
+      title,
+      imageUrl,
+      description
+    };
 
-  createCard(card).then(() => {
-    location.href = '/';
-  });
+    createCard(card).then(() => {
+      location.href = '/';
+    });
+  }
 });
 
 const deleteButton = getElement('.btn-danger');
