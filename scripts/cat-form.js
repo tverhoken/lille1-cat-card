0c6552a522a82a31d358684fdb0b872b07dfc40f
Thomas VERHOKEN <tverhoken@norauto.com>
2018-10-06 10:42:06 +0200

    Add cat card creation

--- scripts/cat-form.js
+++ scripts/cat-form.js
@@ -1,3 +1,24 @@
-import { setElementContent } from './dom-helper.js';
+import { createCard } from './data-service.js';
+import { setElementContent, getElement, addClass } from './dom-helper.js';
+
+getElement('#cardForm').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const title = getElement('#cardTitle').value;
+  const imageUrl = getElement('#cardImage').value;
+  const description = getElement('#cardDescription').value;
+  const card = {
+    title,
+    imageUrl,
+    description
+  };
+
+  createCard(card).then(() => {
+    location.href = '/';
+  });
+});
+
+const deleteButton = getElement('.btn-danger');
+addClass(deleteButton, 'd-none');
 
 setElementContent('#currentYear', new Date().getFullYear());
